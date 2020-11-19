af359425fcda3d2ea9f4bd24d76dbed089aeafec
Route9Library <you@example.com>
2020-11-19 18:16:58 -0500

    prompt 5ish

--- prompt.js
+++ prompt.js
@@ -1,28 +1,35 @@
-// Prompt 1
-function greeting(){
-    console.log("Hey there")
+// // Prompt 1
+// function greeting(){
+//     console.log("Hey there")
+// }
+
+// // Prompt 2
+// function reverse(x)
+// {
+// 	x = x + "";
+// 	return x.split("").reverse().join("");
+// }
+// console.log(reverse(32243));
+
+// // Prompt 3
+// function palindrome(str) {
+//     const re = /[\W_]/g;
+//     const lowRegStr = str.toLowerCase().replace(re, '');
+//     const reverseStr = lowRegStr.split('').reverse().join(''); 
+//     return reverseStr === lowRegStr;
+//   }
+//   palindrome("A man, a plan, a canal. Panama");
+
+// //   Prompt 4
+// function alphaOrder(Order)
+//   {
+// return Order.split('').sort().join('');
+//   }
+// console.log(alphabet_order("webmaster"));
+
+// Prompt 5
+function changeToLowerCase(x){
+ console.log(x.toLowerCase());
 }
 
-// Prompt 2
-function reverse(x)
-{
-	x = x + "";
-	return x.split("").reverse().join("");
-}
-console.log(reverse(32243));
-
-// Prompt 3
-function palindrome(str) {
-    const re = /[\W_]/g;
-    const lowRegStr = str.toLowerCase().replace(re, '');
-    const reverseStr = lowRegStr.split('').reverse().join(''); 
-    return reverseStr === lowRegStr;
-  }
-  palindrome("A man, a plan, a canal. Panama");
-
-//   Prompt 4
-function alphaOrder(Order)
-  {
-return Order.split('').sort().join('');
-  }
-console.log(alphabet_order("webmaster"));
+changeToLowerCase("HELLO")
